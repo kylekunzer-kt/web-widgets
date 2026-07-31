1a481f8e10d4901740389cd1a1649318fcc5c920
Kyle Kunzer <kyle.kunzer@kinetechcloud.com>
2026-07-31 13:45:07 -0500

    feat(datagrid-multi-text-filter): scaffold widget package

---
+++ packages/pluggableWidgets/datagrid-multi-text-filter-web/src/DatagridMultiTextFilter.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from "react";
+import { DatagridMultiTextFilterContainerProps } from "../typings/DatagridMultiTextFilterProps";
+
+export default function DatagridMultiTextFilter(_props: DatagridMultiTextFilterContainerProps): ReactElement {
+    return <div />;
+}
